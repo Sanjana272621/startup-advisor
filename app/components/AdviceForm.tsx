0328b7c1f1fda704222fb6e18aace85ab2731910
Sanjana272621 <sanjana.sureshkumar05@gmail.com>
2025-06-29 18:13:31 +0530

    user prompt

--- app/components/AdviceForm.tsx
+++ app/components/AdviceForm.tsx
@@ -1,6 +1,14 @@
 "use client";
+import {useState} from 'react';
 
 export default function Home() {
+
+  const [userPrompt, setUserPrompt] = useState("");
+
+  function submitPrompt(){
+    console.log("prompt submitted: ", userPrompt);
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
@@ -9,6 +17,8 @@ export default function Home() {
         <label>
           <textarea
             name="postContent"
+            value = {userPrompt}
+            onChange = {(e) => setUserPrompt(e.target.value)}
             rows={4}
             className="w-full border border-gray-300 rounded p-2 resize-none mb-4"
             placeholder="Type your startup idea here..."
@@ -18,6 +28,7 @@ export default function Home() {
         <button
           type="submit"
           className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"
+          onClick = {submitPrompt}
         >
           Submit
         </button>
